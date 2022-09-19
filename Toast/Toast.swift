77aef0f26f9d0b929b005a6771325b0796da2515
yangjianxiang <yangjianxiang@xunyou.com>
2022-09-19 14:19:48 +0800

    5.0.1-v1

--- Toast/Toast.swift
+++ Toast/Toast.swift
@@ -314,7 +314,8 @@ public extension UIView {
         let activityView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: style.activitySize.width, height: style.activitySize.height))
         activityView.backgroundColor = style.activityBackgroundColor
         activityView.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin, .flexibleTopMargin, .flexibleBottomMargin]
-        activityView.layer.cornerRadius = style.cornerRadius
+		activityView.layer.cornerRadius = style.activityCornerRadius
+
         
         if style.displayShadow {
             activityView.layer.shadowColor = style.shadowColor.cgColor
@@ -335,7 +336,7 @@ public extension UIView {
     // MARK: - Private Show/Hide Methods
     
     private func showToast(_ toast: UIView, duration: TimeInterval, point: CGPoint) {
-        toast.center = point
+		toast.center = .init(x: point.x, y: 0)
         toast.alpha = 0.0
         
         if ToastManager.shared.isTapToDismissEnabled {
@@ -349,6 +350,7 @@ public extension UIView {
         self.addSubview(toast)
         
         UIView.animate(withDuration: ToastManager.shared.style.fadeDuration, delay: 0.0, options: [.curveEaseOut, .allowUserInteraction], animations: {
+			toast.center = point
             toast.alpha = 1.0
         }) { _ in
             let timer = Timer(timeInterval: duration, target: self, selector: #selector(UIView.toastTimerDidFinish(_:)), userInfo: toast, repeats: false)
@@ -363,6 +365,7 @@ public extension UIView {
         }
         
         UIView.animate(withDuration: ToastManager.shared.style.fadeDuration, delay: 0.0, options: [.curveEaseIn, .beginFromCurrentState], animations: {
+			toast.center = .init(x: toast.center.x, y: 0)
             toast.alpha = 0.0
         }) { _ in
             toast.removeFromSuperview()
@@ -424,7 +427,7 @@ public extension UIView {
         let wrapperView = UIView()
         wrapperView.backgroundColor = style.backgroundColor
         wrapperView.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin, .flexibleTopMargin, .flexibleBottomMargin]
-        wrapperView.layer.cornerRadius = style.cornerRadius
+		wrapperView.layer.cornerRadius = style.cornerRadius
         
         if style.displayShadow {
             wrapperView.layer.shadowColor = UIColor.black.cgColor
@@ -524,6 +527,7 @@ public extension UIView {
         if let imageView = imageView {
             wrapperView.addSubview(imageView)
         }
+		wrapperView.layer.cornerRadius = style.cornerRadius < 0 ? wrapperHeight / 2.0 : style.cornerRadius
         
         return wrapperView
     }
@@ -678,6 +682,7 @@ public struct ToastStyle {
      */
     public var fadeDuration: TimeInterval = 0.2
     
+	public var activityCornerRadius: CGFloat = 10.0;
     /**
      Activity indicator color. Default is `.white`.
      */
